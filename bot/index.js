04e65ebafb035017fcb45e40e65c6988ea783a54
Николай Бойцов <liniuss75@gmail.com>
2018-03-02 20:33:20 +0200

    update

---
+++ bot/index.js
@@ -0,0 +1,99 @@
+const VK = require("vk-io");
+
+function InjBot (config) {
+    _this = this;
+
+    this.vk = new VK({
+        call: "execute",
+        token: config.access_token
+    })
+    // Commands array
+    this.commands = [];
+    this.utils = require("./utils");
+    this.sets = config;
+
+    _this.vk.api.users.get()
+        .then(function (res) {
+            _this.sets.id = res[0].id;
+            return console.log(`[VK] new account ${res[0].first_name} ${res[0].last_name} | ${res[0].id}`);
+        })
+    
+    this.start = function () {
+        _this.vk.longpoll.start()
+            .then(() => console.log(`[BOT] Longpoll has been started..`))
+        
+        _this.vk.longpoll.on("message", function (message) {
+            if(!message.text || ~message.flags.indexOf("outbox")) return;
+
+            var cmd = _this.commands.find(e => e.pattern.test(message.text));
+            if(!cmd) return;
+            if(cmd.admin && !~config.admins.indexOf(message.user)) return;
+            // Logs
+            console.log(`[MSG] Message from ${message.user}` + (message.chat ? ' | Chat_id: ' + message.chat : '') + `, message: ${message.text.match(/.{0,36}/).join("")}`);
+            // Args
+            message.args = message.text.match(cmd.pattern) || [];
+            // Append / apply
+            message.body = "";
+            message.append = (text) => message.body += text + '\n';
+            message.apply = function (params = {}) {
+                message.reply(message.body, params);
+                message.body = "";
+            }
+            // Plain сообщение, т.е "Имя + текст"
+            message.plain = async function (text, params = {}) {
+                const res = await _this.vk.api.users.get({ 
+                    user_id: message.user
+                })
+
+                return message.send(res[0].first_name + ', ' + text, params);
+            }
+            // Применение функции
+            cmd.func(message, _this);
+        })
+
+        _this.vk.longpoll.on("chat.invite", function (action) {
+            if(action.invite === _this.sets.id) return;
+            _this.vk.api.users.get({
+                user_ids: action.invite
+            }).then(function (res) {
+                return action.send(`Приветствую *id${action.invite} (${res[0].first_name}) в нашей крутой беседе`);
+            })
+        })
+
+        _this.vk.longpoll.on("chat.kick", function (action) {
+            if(action.invite === _this.sets.id) return;
+            return action.send(`Пока :C`);
+        })
+
+        setInterval(async() => {
+            _this.vk.api.friends.getRequests({})
+            .then(res => {
+                if(res.count == 0) return;
+                res.items.map(x => { 
+                    vk.api.call('friends.add', { user_id: x })
+                    .catch(() => { 
+                        vk.api.call('friends.delete', { user_id: x }) 
+                    })
+                });
+            })
+            _this.vk.api.friends.getRequests({out: 1})
+            .then(res => {
+                if(res.count == 0) return;
+                res.items.map(x => { 
+                    vk.api.call('friends.delete', { user_id: x })
+                })
+            })
+        }, 60000)
+    }
+
+    this.on = function (p,d,f,a = false) {
+        _this.commands.push({
+            "pattern": p,                   // Паттерн команды, например /^!test/i
+            "description": d,               // Описание команды
+            "func": f,                      // Функция команды
+            "admin": a                      // Является ли команда администраторкой, по умолчанию false
+        })
+    }
+}
+
+module.exports = InjBot;
---
+++ index.js
@@ -0,0 +1,63 @@
+﻿// Подключение "ядра"
+const Core = require("./bot/index");
+// Создание нового экземпляра
+const bot = new Core({
+    access_token: "",   // Токен
+    admins: [1]         // Айди администраторов
+})
+// Подключение лонгпулла
+bot.start();
+// Пример использования message.plain
+bot.on(/^!test/i, "test -- команда для првоерки бота", function (message) {
+    return message.plain(`я работаю`);
+})
+// Пример использования message.append
+bot.on(/^!(?:online|онлайн)/i, "online -- покажет пользователей онлайн", function (message, {vk}) {
+    vk.api.messages.getChatUsers({
+        chat_id: message.chat,
+        fields: "online"
+    }).then(async function (response) {
+        message.append(`Сейчас в чате:`);
+        await response.map(e => {
+            if(e.id === -152029297) return;
+            if(e.online != 0) message.append(`*id${e.id} (${e.first_name.slice(0,1)}. ${e.last_name})`);
+        })
+        return message.apply({ attachment: "photo435378035_456252104" });
+    })
+})
+// Пример использования message.args + создание админской команды
+bot.on(/^!eval\s(.*)/i, "eval -- исполняет JS код", function (message, core) {
+    let result = eval(message.args[1]);
+    return message.reply (
+        typeof result === "object" ? JSON.stringify(result, null, "&#4448;") : result.toString()
+    )
+}, true)
+// Пример с использованием "utils"
+bot.on(/^!шар\s(.*)/i, "шар <text> -- ответит <<да>> или <<нет>> на ваш вопрос", function (message, {utils}) {
+    return message.plain ( utils.randomPick( ['да', 'нет'] ) )
+})
+// Пример как выводить все команды, без учета админских
+bot.on(/^!help/i, "help -- вывод всех доступных команд", function (message, {commands}) {
+    return message.plain(`список доступных команд:\n` +
+        commands.filter(cmd => !cmd.admin).map(cmd => '!' + cmd.description).join("\n")
+    )
+})
+
+/** Примеры как сделать модульного бота
+
+    # Примитивный, плохой пример:
+        const fs = require("fs");
+
+        fs.readdirSync('./commands').filter(e => e.endsWith('.js')).map(e => {
+            cmd = require(e);
+            bot.on(cmd.pattern, cmd.description, cmd.func, cmd.admin)
+        })
+
+    # Вариант получше:
+        const fs = require("fs");
+
+        fs.readdirSync('./commands').filter(e => e.endsWith('.js')).map(e => {
+            bot.commands.push(e);
+        })
+
+**/
